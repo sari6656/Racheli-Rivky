13a96d13af1228131031214f17f6f41aa99f19bc
rachetan <rachetan@amdocs.com>
2019-12-22 10:14:10 +0200

    editProject

--- ten10ServerSide/ten10ServerSide/Controllers/QuestionsController.cs
+++ ten10ServerSide/ten10ServerSide/Controllers/QuestionsController.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Web.Http;
 using DAL;
+using System.Linq;
+
+
 namespace ten10ServerSide.Controllers
 {
     public class QuestionsController : ApiController
     {
+      
         public IEnumerable<Questions> Get()
         {
             using(questionnaireEntities entities = new questionnaireEntities())
             {
-                return entities.Questions.questionDesc.toList();
+                return entities.Questions.ToList();
+            }
+        }
+        public Teachers Get(int id)
+        {
+            using (questionnaireEntities entities = new questionnaireEntities())
+            {
+                return entities.Teachers.FirstOrDefault(e=>e.questionnaire==id);
             }
         }
     }
